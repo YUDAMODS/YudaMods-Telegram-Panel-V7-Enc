b85852a92ca2a72504e687f671842625ad6c1403
YUDAMODS <abdulsamid317@gmail.com>
2024-03-12 12:39:44 +0700

    github

---
+++ settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: 'your_bot_token',
+  pterodactylApiUrl: 'your_api_url',
+  pterodactylApiKey: 'your_api_key',
+  ownerContact: '0',
+  thumbPath: 'thumb.png',
+  youtubeLink: 'https://youtube.com/@YUDAMODS',
+};
